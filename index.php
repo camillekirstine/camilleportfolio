5f54f0c0f438cdc6a68197dcde35df159c036cf5
Charlie <camille@camillekirstine.com>
2022-08-24 13:43:20 +0200

    created bootstrap files

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
 
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -20,5 +21,7 @@
 <body>
 
 
+
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
