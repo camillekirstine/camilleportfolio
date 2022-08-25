45871c191bc3df07d6c2c1946c54024b9ed82b91
Charlie <camille@camillekirstine.com>
2022-08-25 12:07:45 +0200

    finished welcomepage, added custom colors and fonts, made a fadeIn effect with javascript

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<footer>
+    <div class="mx-auto col text-center pt-3">
+        <a href="https://www.linkedin.com/in/camille-kirstine-larsson-bb9859226/"><i class="fa-brands fa-linkedin"></i></a>
+        <a href="https://github.com/camillekirstine"><i class="fa-brands fa-square-github"></i></a>
+    </div>
+
+
+    <div class="text-center col mx-auto">
+        <p>Graphic design, webdesign and front-end development</p>
+    </div>
+
+    <div class="text-center col mx-auto pb-3">
+        <p>Copyright Camille Kirstine Larsson 2022</p>
+    </div>
+
+</footer>
--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 
 <!DOCTYPE html>
-<html lang="da">
+<html lang="da" xmlns="http://www.w3.org/1999/html">
 
 <head>
     <meta charset="utf-8">
 
-    <title>Sigende titel</title>
+    <title>Camille Kirstine Studio</title>
 
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
@@ -13,16 +13,58 @@
 
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.typekit.net/wtu5hgz.css">
+
+    <script src="https://kit.fontawesome.com/f5df81b727.js" crossorigin="anonymous"></script>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
 </head>
 
 
 <body>
+<main id="body" style="opacity: 0;">
+    <div class="container">
+         <div class="row">
+             <div class="col">
+                 <img class="p-5 mx-auto" src="images/camillekitopbtm.png">
+                </div>
+         </div>
 
-<div class="container-fluid welcomepage">
+            <div class="row mb-5">
+                 <button type="button" class="btnfront col-6 text-nowrap btn-lg mx-auto">
+                     <a href="#" class="mx-auto">ENTER</a>
+                </button>
+            </div>
 
-</div>
+        </div>
+</main>
+
+<?php include "includes/footer.php"; ?>
+
+<script type="text/javascript">
+    var opacity = 0;
+    var intervalID = 0;
+    window.onload = fadeIn;
+
+    function fadeIn() {
+        setInterval(show, 200);
+    }
+
+    function show() {
+        var body = document.getElementById("body");
+        opacity = Number(window.getComputedStyle(body)
+            .getPropertyValue("opacity"));
+        if (opacity < 1) {
+            opacity = opacity + 0.1;
+            body.style.opacity = opacity
+        } else {
+            clearInterval(intervalID);
+        }
+    }
+</script>
+
 
 
 
