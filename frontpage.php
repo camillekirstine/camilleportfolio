6e354da1af86d3f4844c992a9d7c384c772a9c65
Charlie <camille@camillekirstine.com>
2022-08-25 20:05:04 +0200

    finished info box, added buttons, opacity and adjusted the size of the buttons

--- frontpage.php
+++ frontpage.php
@@ -35,19 +35,36 @@
 
 <?php include "includes/navbar.php" ?>
 
-<main class="container">
-    <div class="row">
+<main class="container text-center">
+    <div class="row p-3">
         <div class="col-12 col-lg-6 mx-auto">
             <img class="presentationavatar mx-auto pt-1" src="images/presentation-avatar.png">
         </div>
         <div class="col-12 col-lg-6 mx-auto infobox bg-lightBlue">
             <h1 class="text-center pt-3 frontwelcome">WELCOME!</h1>
+
+            <p class="text-center">You've entered my awesome online portfolio!<br>
+            Here you will be able to explore my work, learn more about me and send me an e-mail, if you're feeling up for that!</p>
+
+            <h5 class="text-center pt-3">You now have the following options:</h5>
+
+         <div class="row mx-auto">
+             <div class="col col-lg-12"><button type="button" class="btninfo col-6 text-nowrap btn-lg mx-auto justify-content-center m-5"><a href="#">Check out my work</a></button></div>
+
+            <div class="col col-lg-12"><button type="button" class="btninfo col-6 text-nowrap btn-lg mx-auto justify-content-center m-5"><a href="#"> Learn more about me</a></button></div>
+
+            <div class="col col-lg-12"><button type="button" class="btninfo col-6 text-nowrap btn-lg mx-auto justify-content-center m-5"><a href="#">See my contact information</a> </button></div>
+         </div>
+
+
         </div>
     </div>
 </main>
 
 
 <?php include "includes/footer.php"; ?>
+
+
 
 
 
