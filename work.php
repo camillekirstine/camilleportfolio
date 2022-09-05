bb3ef304bb0cba29f24e29d5098fc532b05490e3
Charlie <camille@camillekirstine.com>
2022-09-05 14:23:15 +0200

    added cards to work page, added effects to these cards

--- work.php
+++ work.php
@@ -34,7 +34,26 @@
 
 <?php include "includes/navbar.php" ?>
 
-<main>
+<main class="container-fluid">
+    <div class="container">
+        <div class="row mx-auto">
+            <div class="col-12 col-lg-6 bg-lightBlue infobox mx-auto">
+                <h1 class="text-center p-3 frontwelcome">PORTFOLIO</h1>
+                <p class="text-center pb-3">This is the overview of my portfolio, here you can see some of the projects I've worked on.
+                This site is ever expanding, and I will add new projects along the way - so stay tuned!</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row mx-auto justify-content-center">
+            <div class="logolink col-12 col-lg-2 col-md-4 m-3 pt-5"><a href="#"><img alt="sommerlyd logo" src="images/logo-02.png"></a> </div>
+            <div class="logolink col-12 col-lg-2 col-md-4 m-3 pt-5 pb-5"><a href="#"><img alt="gilleleje isen logo" src="images/GIlogo.png"></a></div>
+            <div class="logolink col-12 col-lg-2 col-md-4 m-3 pt-5 pb-5"><a href="#"><img  alt="chocolate club logo" src="images/CClogo.png"></a></div>
+            <div class="logolink col-12 col-lg-2 col-md-4 m-3 pb-5 pt-5"><a href="#"><h1 class="text-center pb-5 pt-5
+           portfoliolink">Various<br>Projects</h1></a></div>
+        </div>
+    </div>
 
 </main>
 
