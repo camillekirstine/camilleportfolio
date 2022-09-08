365a9062399347be7897dbc81e3d93076a388f08
Charlie <camille@camillekirstine.com>
2022-09-08 15:17:49 +0200

    made error page and htaccess file

---
+++ error404.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+
+<html lang="da">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <title>Error 404</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Camille Kirstine Larsson">
+    <meta name="copyright" content="Copyright Camille Kirstine Larsson">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.typekit.net/wtu5hgz.css">
+
+    <script src="https://kit.fontawesome.com/f5df81b727.js" crossorigin="anonymous"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:CamilleKirstine" content="CamilleKirstine">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="#">
+    <meta property="og:image" content="#">
+    <meta property="og:description" content="Online portfolio and resumé for Camille Kirstine. Graphic Design, Web Design and Front-end Development.">
+    <meta property="og:locale" content="da_DK">
+    <link rel="shortcut icon" href="images/camilleki.png" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<?php include "includes/navbar.php" ?>
+
+<main>
+    <div class="container">
+        <div class="row mx-auto">
+            <div class="col-12 col-lg-6 bg-lightBlue infobox mx-auto">
+                <h1 class="text-center p-3 frontwelcome">WHOOPSIE!</h1>
+                <p class="text-center">Awwww sad! Something seems to have gone wrong! That really sucks!</p>
+                <p class="text-center">Don't worry though! You just have to go back to the <a href="frontpage.php">frontpage</a>, and you should be good to go. </p>
+                <img class="mx-auto" src="images/camillesad.png">
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php include "includes/footer.php" ?>
+
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
